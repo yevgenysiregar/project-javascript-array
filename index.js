ba1d037f90b7c54a40ca1de017bb91767fce044a
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-04-04 17:49:56 +0700

    Add index.js and add text inside

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log([0, 1, 2]);
+console.log([0, 1, 2, 3]);
+console.log(["Yevgeny", 500, false]);
+
+const person = ["Yevgeny", 500, false];
+
+console.log(person);
+console.log(person[0]);
+console.log(person[1]);
+console.log(person[2]);
+
+alert(person);
+alert(person[0]);
+alert(person[1]);
+alert(person[2]);
